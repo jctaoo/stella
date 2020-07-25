7beec4b9d83882b51e36cef6fa75008a2ce20385
jctaoo <jctaoo@outlook.com>
2020-07-26 00:08:12 +0800

    add not found page

--- src/Pages/NotFoundPage/NotFoundPage.tsx
+++ src/Pages/NotFoundPage/NotFoundPage.tsx
@@ -5,7 +5,17 @@ import BasePage from "../BasePage";
 function NotFoundPage() {
   return (
     <BasePage id="not-found-page">
-      FUCK
+      <div id="not-found-page-content">
+        <div id="not-found-page-explanation">
+          <h1>Page Not Found</h1>
+          <p>Why not use the search function to view other content?</p>
+        </div>
+      </div>
+      <div id="not-found-page-background">
+        <span id="not-found-page-background-label">
+          <p id="not-found-page-background-label-text">404</p>
+        </span>
+      </div>
     </BasePage>
   );
 }
