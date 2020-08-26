d4f943888fde1e809f011a3b9be89c7b4b8ae420
jctaoo <jctaoo@outlook.com>
2020-08-26 10:57:40 +0800

    integrated with redux

--- src/layout/app-container/app-container.tsx
+++ src/layout/app-container/app-container.tsx
@@ -3,17 +3,21 @@ import "./app-container.scss"
 import { AnimatePresence } from "framer-motion";
 import MobileNavigationBar from "../../components/mobile-navigation-bar/mobile-navigation-bar";
 import NavigationBar from "../../components/navigation-bar/navigation-bar";
+import { Provider } from "react-redux";
+import store from "../../state";
 
 function AppContainer({children}: {children: ReactElement}) {
   return (
     <div id="stella-container" style={{ display: "contents" }}>
-      <MobileNavigationBar />
-      <div id="home-page">
-        <NavigationBar />
-        <AnimatePresence>
-          { children }
-        </AnimatePresence>
-      </div>
+      <Provider store={store}>
+        <MobileNavigationBar />
+        <div id="home-page">
+          <NavigationBar />
+          <AnimatePresence>
+            { children }
+          </AnimatePresence>
+        </div>
+      </Provider>
     </div>
   );
 }
---
+++ src/state.ts
@@ -0,0 +1,56 @@
+import { createStore, applyMiddleware, Action } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import { Reducer } from "redux";
+import AppState from "./models/app-state";
+
+// #==================== action ====================#
+
+export const CHANGE_PATHNAME_ACTION_ID = Symbol("CHANGE_PATHNAME_ACTION_ID")
+
+export class Actions {
+
+  static createChangePathnameAction(destination: string): ChangePathnameAction {
+    return {
+      type: CHANGE_PATHNAME_ACTION_ID,
+      destination,
+    }
+  }
+
+}
+
+export interface ChangePathnameAction extends Action<Symbol> {
+  type: Symbol
+  destination: string
+}
+
+// #==================== action ====================#
+
+
+// #==================== reducer ====================#
+
+const initialState: AppState = { currentPathname: "/" }
+const appRducer: Reducer<AppState, Action<Symbol>> = (state = initialState, action): AppState => {
+  if (action.type === CHANGE_PATHNAME_ACTION_ID) {
+    const changePathnameAction = action as ChangePathnameAction;
+    return {
+      ...state,
+      currentPathname: changePathnameAction.destination,
+    };
+  }
+  return state;
+}
+
+// #==================== reducer ====================#
+
+
+// #==================== store ====================#
+
+const store = createStore(appRducer, composeWithDevTools(
+  applyMiddleware(thunk)
+));
+
+// #==================== store ====================#
+
+
+export default store;
