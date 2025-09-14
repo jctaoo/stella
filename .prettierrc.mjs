a3209732587db1567161dab9cc18189488c9080b
jctaoo <jctaoo@outlook.com>
2025-09-14 22:53:56 +0800

    Initial Setup

---
+++ .prettierrc.mjs
@@ -0,0 +1,5 @@
+/** @type {import("prettier").Config} */
+export default {
+  printWidth: 120,
+  plugins: ["prettier-plugin-astro"],
+};
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 // @ts-check
 
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-      plugins: [tailwindcss()],
-	},
-
-  integrations: [react()],
+    plugins: [tailwindcss()],
+  },
+  integrations: [react(), mdx()],
 });
